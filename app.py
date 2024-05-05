046739d5d2d015dd6a18eeb9d1967e71fb8c5b6d
Nicholas Gibson <nicholasgibson2@gmail.com>
2024-05-05 15:27:53 -0700

    minor refactor

--- app.py
+++ app.py
@@ -6,69 +6,35 @@ def persist_vals(cur_key, prev_key):
     st.session_state[prev_key] = st.session_state[cur_key]
 
 
-def artists_dropdown(df):
-    artists = df["Artist"].unique()
-    artists.sort()
+def dynamic_dropdown(df, column, dependencies):
+    for dep_column, dep_values in dependencies.items():
+        if dep_values:
+            df = df[df[dep_column].isin(dep_values)]
 
-    prev_artists = st.session_state.get("prev_artists", [])
-    prev_artists = [artist for artist in prev_artists if artist in artists]
-    st.session_state.prev_artists = prev_artists
+    options = sorted(df[column].unique())
 
-    return st.sidebar.multiselect(
-        "Artist",
-        options=artists,
-        default=prev_artists,
+    prev_selections = st.session_state.get(f"prev_{column}", [])
+    prev_selections = [s for s in prev_selections if s in options]
+
+    st.session_state[f"prev_{column}"] = prev_selections
+
+    selected = st.sidebar.multiselect(
+        label=column,
+        options=options,
+        default=prev_selections,
         on_change=persist_vals,
-        args=("cur_artists", "prev_artists"),
-        key="cur_artists",
+        args=(f"cur_{column}", f"prev_{column}"),
+        key=f"cur_{column}",
     )
 
-
-def titles_dropdown(df, artists):
-    filtered_df = df[df["Artist"].isin(artists)] if artists else df
-    titles = filtered_df["Title"].unique()
-    titles.sort()
-
-    prev_titles = st.session_state.get("prev_titles", [])
-    prev_titles = [title for title in prev_titles if title in titles]
-    st.session_state.prev_titles = prev_titles
-
-    return st.sidebar.multiselect(
-        "Title",
-        options=titles,
-        default=prev_titles,
-        on_change=persist_vals,
-        args=("cur_titles", "prev_titles"),
-        key="cur_titles",
-    )
+    return selected
 
 
-def reference_number_dropdown(df, artists, titles):
-    if artists and not titles:
-        filtered_df = df[df["Artist"].isin(artists)]
-    elif titles:
-        filtered_df = (
-            df[(df["Artist"].isin(artists)) & (df["Title"].isin(titles))]
-            if artists
-            else df[df["Title"].isin(titles)]
-        )
-    else:
-        filtered_df = df
-
-    refs = filtered_df["Reference Number"].unique()
-    refs.sort()
-
-    prev_refs = st.session_state.get("prev_refs", [])
-    prev_refs = [ref for ref in prev_refs if ref in refs]
-    st.session_state.prev_refs = prev_refs
-
-    return st.sidebar.multiselect(
-        "Reference Number",
-        options=refs,
-        default=prev_refs,
-        on_change=persist_vals,
-        args=("cur_refs", "prev_refs"),
-        key="cur_refs",
+def filter_condition(df, column_name, filter_list):
+    return (
+        df[column_name].isin(filter_list)
+        if filter_list
+        else pd.Series([True] * len(df), index=df.index)
     )
 
 
@@ -77,26 +43,16 @@ def read_file(file):
     return pd.read_csv(file)
 
 
+@st.cache_data
+def load_css(file_name):
+    with open(file_name, "r") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
 def main():
     st.set_page_config(page_title="Laser Juke Explorer", layout="wide")
     st.image("laserjuke.png")
-
-    st.markdown(
-        f"""
-            <style>
-                   .st-emotion-cache-z5fcl4 {{
-                        padding-top: 2.5rem;
-                    }}
-                   .st-emotion-cache-9tg1hl {{
-                        padding-top: 2.5rem;
-                    }}
-                   .st-emotion-cache-18ni7ap {{
-                        height: 0rem;
-                    }}
-            </style>
-        """,
-        unsafe_allow_html=True,
-    )
+    load_css("style.css")
 
     discs_df = read_file("tino_discs.csv")
     titles_df = read_file("tino_titles.csv")
@@ -104,60 +60,26 @@ def main():
 
     df = pd.merge(discs_df, titles_df, on="Reference Number")
 
-    df["Month"] = df["Reference Number"].str.extract(r"\d{2}\.\d{2}\.(\d{2}).*")
-    df["Month"] = pd.to_numeric(df["Month"], errors="coerce").astype("Int64")
-
-    df.dropna(subset=["Month"], inplace=True)
-
-    df["Year"] = df["Year"].fillna(1900).astype(int)
-    df["Year"] = df["Year"].astype(int)
-    df["Month"] = df["Month"].astype(int)
-
-    df["Date"] = pd.to_datetime(
-        df["Year"].astype(int).astype(str)
-        + "-"
-        + df["Month"].fillna(1).astype(int).astype(str)
-        + "-01",
-        format="%Y-%m-%d",
-        errors="coerce",
-    )
-    df.loc[df["Month"].isna(), "Date"] = pd.to_datetime(
-        df["Year"].astype(int).astype(str) + "-01-01"
-    )
-
     column_order = ["Reference Number", "Year", "Artist", "Title", "Owned"]
-
     column_config = {"Year": st.column_config.NumberColumn(format="%f")}
 
-    df["Owned"] = df["Reference Number"].apply(
-        lambda x: any(
-            x.startswith(owned_ref) for owned_ref in owned_discs_df["Reference Number"]
-        )
-    )
+    df["Owned"] = df["Reference Number"].isin(owned_discs_df["Reference Number"])
 
     if st.sidebar.checkbox("Owned"):
-        df = df[df["Owned"] == True]
+        df = df[df["Owned"]]
 
-    artists = artists_dropdown(df)
-    titles = titles_dropdown(df, artists)
-    ref_number = reference_number_dropdown(df, artists, titles)
+    artists = dynamic_dropdown(df, "Artist", {})
+    titles = dynamic_dropdown(df, "Title", {"Artist": artists})
+    refs = dynamic_dropdown(
+        df, "Reference Number", {"Artist": artists, "Title": titles}
+    )
 
-    ref_condition = (
-        df["Reference Number"].isin(ref_number)
-        if ref_number
-        else pd.Series([True] * len(df), index=df.index)
-    )
-    artist_condition = (
-        df["Artist"].isin(artists)
-        if artists
-        else pd.Series([True] * len(df), index=df.index)
-    )
-    title_condition = (
-        df["Title"].isin(titles)
-        if titles
-        else pd.Series([True] * len(df), index=df.index)
-    )
+    artist_condition = filter_condition(df, "Artist", artists)
+    title_condition = filter_condition(df, "Title", titles)
+    ref_condition = filter_condition(df, "Reference Number", refs)
+
     filtered_df = df[ref_condition & artist_condition & title_condition]
+
     st.dataframe(
         filtered_df,
         hide_index=True,
