06875aed0fb878bd7ddeab467217c0232eec5f0d
Nicholas Gibson <nicholasgibson2@gmail.com>
2024-05-05 20:50:41 -0700

    adding functionality for custom lists

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ import pandas as pd
 from pathlib import Path
 
 
+custom_list_columns = ["Reference Number", "Artist", "Title"]
+
+
+def reset_last_change():
+    st.session_state.last_diffs = {}
+
+
 def persist_vals(cur_key, prev_key):
     st.session_state[prev_key] = st.session_state[cur_key]
+    reset_last_change()
 
 
 def dynamic_dropdown(df, column, dependencies):
@@ -39,7 +47,7 @@ def filter_condition(df, column_name, filter_list):
     )
 
 
-@st.cache_data
+# @st.cache_data
 def read_csv_file(file):
     return pd.read_csv(file)
 
@@ -59,20 +67,66 @@ def custom_list_name_format(filename):
     return Path(filename).stem.replace("_", " ").title()
 
 
+def save_custom_lists():
+    for custom_list in st.session_state.custom_lists.values():
+        custom_list["df"].to_csv(
+            "./custom_lists/" + custom_list["filename"], columns=custom_list_columns
+        )
+
+
 def custom_list():
     custom_list_dir = "./custom_lists"
     files = custom_list_files(custom_list_dir)
+
+    if "custom_lists" not in st.session_state:
+        custom_lists = {}
+        for file in files:
+            pretty_name = custom_list_name_format(file)
+            df = read_csv_file(f"{custom_list_dir}/{file}")
+            custom_lists[pretty_name] = {"filename": file, "df": df}
+        st.session_state.custom_lists = custom_lists
 
     selected = st.sidebar.selectbox(
         "List",
         files,
         format_func=custom_list_name_format,
         index=files.index("juke_night.csv"),
+        on_change=reset_last_change,
     )
 
     selected_str = custom_list_name_format(selected)
-    selected_df = read_csv_file(f"{custom_list_dir}/{selected}")
+    selected_df = st.session_state.custom_lists[selected_str]["df"]
+
     return selected_str, selected_df
+
+
+def update_custom_val():
+    last_diffs = st.session_state.get("last_diffs", {})
+    cur_diffs = st.session_state.cur_diffs["edited_rows"]
+
+    for key, val in cur_diffs.items():
+        if (key not in last_diffs) or (last_diffs[key] != val):
+            updated_index = key
+            updated_row = val
+            break
+
+    row = st.session_state.diffs_df.iloc[[updated_index]][custom_list_columns]
+
+    df_name, updated_val = next(iter(updated_row.items()))
+    custom_df = st.session_state.custom_lists[df_name]["df"]
+
+    if updated_val:
+        updated_df = pd.concat([custom_df, row], ignore_index=True)
+    else:
+        matched_df = custom_df.merge(
+            row, on=custom_list_columns, how="left", indicator=True
+        )
+        updated_df = matched_df[matched_df["_merge"] == "left_only"].drop(
+            columns="_merge"
+        )
+    st.session_state.custom_lists[df_name]["df"] = updated_df
+
+    st.session_state.last_diffs = cur_diffs
 
 
 def main():
@@ -88,14 +142,19 @@ def main():
 
     st_sidebar = st.sidebar.container()
     df["Owned"] = df["Reference Number"].isin(owned_df["Reference Number"])
-    if st_sidebar.checkbox("Owned"):
+    if st_sidebar.checkbox("Owned", on_change=reset_last_change):
         df = df[df["Owned"]]
 
     custom_name, custom_df = custom_list()
-    df[custom_name] = df["Reference Number"].isin(custom_df["Reference Number"])
 
-    if st.sidebar.checkbox("Filter"):
+    common_columns = custom_list_columns
+    df = df.merge(custom_df, on=common_columns, how="left", indicator=True)
+    df[custom_name] = df["_merge"] == "both"
+    df.drop(columns=["_merge"], inplace=True)
+
+    if st.sidebar.checkbox("Filter", on_change=reset_last_change):
         df = df[df[custom_name]]
+    st.sidebar.button("Save", on_click=save_custom_lists)
 
     with st_sidebar:
         artists = dynamic_dropdown(df, "Artist", {})
@@ -116,12 +175,15 @@ def main():
     column_order = ["Reference Number", "Year", "Artist", "Title", "Owned", custom_name]
     column_config = {"Year": st.column_config.NumberColumn(format="%f")}
 
-    st.dataframe(
+    st.session_state.diffs_df = st.data_editor(
         filtered_df,
+        disabled=column_order[:-1],
         hide_index=True,
         use_container_width=True,
         column_order=column_order,
         column_config=column_config,
+        on_change=update_custom_val,
+        key="cur_diffs",
     )
 
 
